57efbf2cf8f506ede8d6646aba9bee4945f0decc
Bhagyashri Tamrakar <86471670+BST82@users.noreply.github.com>
2022-06-30 21:38:59 +0530

    second commit

--- vector.cpp
+++ vector.cpp
@@ -3,14 +3,44 @@
 using namespace std;
 
 int main(){
-    int n,a;
+
+
+ // take the input without using auto 
+
+ /*** 
+  int n,a;
     cin>>n;
  vector<int>v1;
- for(int i=0;i<n;i++ ){
+   
+for(int i=0;i<n;i++ ){
 cin>>a;
 v1.emplace_back(a);
  }
- for(auto &it:v1){
-cout<<it<<" ";
+ ****/
+
+// give output without using auto keyword 
+
+/***
+  for(int i=0;i<n;i++ ) {
+cout<<v1.at(i)<<" ";
  }
+
+***/
+
+// take input using auto key word 
+int n,a;
+cin>>n;
+vector<int>s;
+for(int i=0;i<n;i++ ){
+cin>>a;
+s.push_back(a);
+ }
+
+//  for (auto it=s.begin();it!=s.end();it++){
+//         cout<<*it<<" ";
+//     }
+
+for(auto &it : s){
+    cout<<it<<" ";
+}
 }
